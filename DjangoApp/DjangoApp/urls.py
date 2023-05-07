c2a9d7a2af6fb1d843d1a1a017ae2bf7c2eaca0e
MSI\mifas <michal.zielonka77@gmail.com>
2023-05-07 13:32:25 +0200

    changes for heroku deployment

--- DjangoApp/DjangoApp/urls.py
+++ DjangoApp/DjangoApp/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-import settings
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import url
 from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('ScriptLanguagesApp.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
