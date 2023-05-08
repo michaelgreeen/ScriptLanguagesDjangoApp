96130bf2099b22ab88eafd59be43ae073bf94926
MSI\mifas <michal.zielonka77@gmail.com>
2023-05-08 17:59:51 +0200

    translate

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -128,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-LANGUAGE_CODE = 'en-us'  # or other appropriate code
+LANGUAGE_CODE = 'pl'  # or other appropriate code
 USE_I18N = True
 USE_L10N = True
 TIME_ZONE = 'UTC'
